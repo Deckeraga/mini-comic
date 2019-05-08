750b138edeb7be1059edb901545f07d5536f1704
Alec Decker <deckeraga@gmail.com>
2019-05-07 17:43:33 -0700

    Clean up comic file ingestion code, add auto-formatting

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,8 +1,8 @@
-import { remote } from 'electron';
-import temp = require('temp');
-import unrar = require('node-unrar');
-import unzip = require('unzip');
-import * as fs from 'fs';
+import { remote } from "electron";
+import temp = require("temp");
+import unrar = require("node-unrar");
+import unzip = require("unzip");
+import * as fs from "fs";
 
 const currentWindow = remote.getCurrentWindow();
 
@@ -10,8 +10,8 @@ const LEFT_ARROW = "ArrowLeft";
 const RIGHT_ARROW = "ArrowRight";
 
 enum Mode {
-    READER,
-    NOCOMIC
+  READER,
+  NOCOMIC
 }
 
 // Current page
@@ -23,167 +23,175 @@ let pageList = new Array();
 temp.track();
 
 // Directory for unzipped/rar'd comics
-const tempDirectory = temp.mkdirSync()
+const tempDirectory = temp.mkdirSync();
 
 // Sub Directory
-let aSubDirectory = '';
+let aSubDirectory = "";
+
+function getComicDirectory(): string {
+  return tempDirectory + aSubDirectory.replace(/:$/, "");
+}
 
 // Utility to retrieve file extension
 function getExtension(filename: string): string {
-    const i = filename.lastIndexOf('.');
-    return (i < 0) ? '' : filename.substr(i);
+  const i = filename.lastIndexOf(".");
+  return i < 0 ? "" : filename.substr(i);
 }
 
 // Revert to previous page
 function nextPage(): void {
-    const newPage = Math.min(pageList.length - 1, currentPage + 1);
-    setPage(newPage);
+  const newPage = Math.min(pageList.length - 1, currentPage + 1);
+  setPage(newPage);
 }
 
 // Advance to the next page
 function prevPage(): void {
-    const newPage = Math.max(0, currentPage - 1);
-    setPage(newPage);
+  const newPage = Math.max(0, currentPage - 1);
+  setPage(newPage);
 }
 
 // Load current page and update index
 function setPage(thePage): void {
-    currentPage = thePage;
-    const page: HTMLImageElement = document.querySelector('#page')
-    page.src = tempDirectory + aSubDirectory + '/' + pageList[currentPage];
-    onPageChange();
-    updatePageStatus();
+  currentPage = thePage;
+  const page: HTMLImageElement = document.querySelector("#page");
+  page.src = getComicDirectory() + "/" + pageList[currentPage];
+  onPageChange();
+  updatePageStatus();
 }
 
 function onPageChange(): void {
-    const img: HTMLImageElement = document.querySelector("#page");
-    try{
-        currentWindow.setAspectRatio(img.naturalWidth / img.naturalHeight, undefined);
-    } catch {
-        console.log("error");
-    }
+  const img: HTMLImageElement = document.querySelector("#page");
+  try {
+    currentWindow.setAspectRatio(
+      img.naturalWidth / img.naturalHeight,
+      undefined
+    );
+  } catch {
+    console.log("error");
+  }
 }
 
 // Run on startup
 function init(): void {
-    currentWindow.setSize(375, 210);
-    setMode(Mode.NOCOMIC);
+  currentWindow.setSize(375, 210);
+  setMode(Mode.NOCOMIC);
 
-    // Drag handler
-    document.ondragover = document.ondrop = (e: DragEvent) => {
-        e.preventDefault()
+  // Drag handler
+  document.ondragover = document.ondrop = (e: DragEvent) => {
+    e.preventDefault();
+  };
+
+  // Drop handler
+  document.body.ondrop = (e: DragEvent) => {
+    e.preventDefault();
+    for (const file of e.dataTransfer.files) {
+      loadComic(file);
+      break;
     }
+  };
 
-    // Drop handler
-    document.body.ondrop = (e: DragEvent) => {
-        e.preventDefault();
-        for (const file of e.dataTransfer.files) {
-            loadComic(file);
-            break;
-        }
-    }
+  // Button listeners
+  document.querySelector("#forward").addEventListener("click", () => {
+    nextPage();
+  });
 
-    // Button listeners
-    document.querySelector('#forward').addEventListener('click', () => {
-        nextPage();
-    });
+  document.querySelector("#back").addEventListener("click", () => {
+    prevPage();
+  });
 
-    document.querySelector('#back').addEventListener('click', () => {
+  // Keyboard listeners
+  document.addEventListener("keydown", (event: KeyboardEvent) => {
+    switch (event.key) {
+      case LEFT_ARROW:
         prevPage();
-    });
-
-    // Keyboard listeners
-    document.addEventListener('keydown', (event: KeyboardEvent) => {
-        switch (event.key) {
-            case LEFT_ARROW:
-                prevPage();
-                break;
-            case RIGHT_ARROW:
-                nextPage();
-                break;
-        }
-    });
+        break;
+      case RIGHT_ARROW:
+        nextPage();
+        break;
+    }
+  });
 }
 
 // Update the page status indicator
 function updatePageStatus(): void {
-    document.querySelector('#page-status').textContent = (currentPage + 1) + " / " + (pageList.length);
+  document.querySelector("#page-status").textContent =
+    currentPage + 1 + " / " + pageList.length;
 }
 
 // Set application mode
-function setMode(mode : Mode): void {
-    switch(mode) {
-        case Mode.READER:
-            getNoComic().style.display = "none";
-            getPageHolder().style.display = "";
-            getButtonHolder().style.display = "";
-            break;
-        case Mode.NOCOMIC:
-            getNoComic().style.display = "";
-            getPageHolder().style.display = "none";
-            getButtonHolder().style.display = "none";
-            break;
-    }
+function setMode(mode: Mode): void {
+  switch (mode) {
+    case Mode.READER:
+      getNoComic().style.display = "none";
+      getPageHolder().style.display = "";
+      getButtonHolder().style.display = "";
+      break;
+    case Mode.NOCOMIC:
+      getNoComic().style.display = "";
+      getPageHolder().style.display = "none";
+      getButtonHolder().style.display = "none";
+      break;
+  }
 }
 
-function getNoComic() : HTMLElement {
-    return document.querySelector('#no-comic');
+function getNoComic(): HTMLElement {
+  return document.querySelector("#no-comic");
 }
 
-function getPageHolder() : HTMLElement {
-    return document.querySelector('#page-holder');
+function getPageHolder(): HTMLElement {
+  return document.querySelector("#page-holder");
 }
 
-function getButtonHolder() : HTMLElement {
-    return document.querySelector('#button-holder');
+function getButtonHolder(): HTMLElement {
+  return document.querySelector("#button-holder");
+}
+
+function initComicDirectory(theSubDirectory: string): string {
+  aSubDirectory = "/" + theSubDirectory;
+
+  const dir = getComicDirectory();
+
+  if (fs.existsSync(dir)) {
+    fs.rmdirSync(dir);
+  }
+  fs.mkdirSync(dir);
+
+  return dir;
 }
 
 // Initialize and load a new comic
 function loadComic(file): void {
-    if (file !== undefined) {
-        const filepath = file.path;
-        const filename = file.name;
+  if (file !== undefined) {
+    const filepath = file.path;
+    const filename = file.name;
 
-        // Handle .cbz (zipped comics)
-        if (getExtension(filepath) === '.cbz') {
-            aSubDirectory = '/' + filename.replace('.cbz', '');
-            const aDirectory = tempDirectory + aSubDirectory;
+    pageList = [];
 
-            if (fs.existsSync(aDirectory)) {
-                fs.rmdirSync(aDirectory);
-            }
-            fs.mkdirSync(aDirectory);
+    // Handle .cbz (zipped comics)
+    if (getExtension(filepath) === ".cbz") {
+      const dir = initComicDirectory(filename.replace(".cbz", ""));
 
-            pageList = [];
-
-            fs.createReadStream(filepath)
-                .pipe(unzip.Extract({ path: tempDirectory + "/"}))
-                .on('close', () => {
-                    fs.readdirSync(aDirectory).forEach((f) => pageList.push(f));
-                    setPage(0);
-                });
-        }
-
-        if (getExtension(filepath) === '.cbr') {
-            aSubDirectory = '/' + filename.replace('.cbr', '');
-            const aDirectory = tempDirectory + aSubDirectory;
-
-            if (fs.existsSync(aDirectory)) {
-                fs.rmdirSync(aDirectory);
-            }
-            fs.mkdirSync(aDirectory);
-
-            pageList = [];
-
-            var archive = new unrar(filepath);
-            
-            archive.extract(aDirectory, null, () => {
-                fs.readdirSync(aDirectory).forEach((f) => pageList.push(f));
-                setPage(0);
-            });
-        }
-        setMode(Mode.READER);
+      fs.createReadStream(filepath)
+        .pipe(unzip.Extract({ path: tempDirectory + "/" }))
+        .on("close", () => {
+          fs.readdirSync(dir).forEach(f => pageList.push(f));
+          setPage(0);
+        });
     }
+
+    // Handle .cbr (rarred comics)
+    if (getExtension(filepath) === ".cbr") {
+      const dir = initComicDirectory(filename.replace(".cbr", ""));
+
+      const archive = new unrar(filepath);
+
+      archive.extract(dir, null, () => {
+        fs.readdirSync(dir).forEach(f => pageList.push(f));
+        setPage(0);
+      });
+    }
+    setMode(Mode.READER);
+  }
 }
 
 init();
